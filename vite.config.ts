72a5d439b8fb36c80e6dd7e8ed5a707759f472b5
Veritas <ntantardini@gmail.com>
2025-06-29 11:58:05 -0700

    3

--- vite.config.ts
+++ vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   build: {
     outDir: path.resolve(import.meta.dirname, "dist"),
     emptyOutDir: true,
-    base: "/temp_website/",
+    base: "./",
   },
   server: {
     fs: {
